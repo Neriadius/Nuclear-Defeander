44527e454c32e3e37bf0f0a37ea28a5ae61f1970
Neriadius <darin1980museev@gmail.com>
2026-06-11 16:40:00 +0500

    Explosion SOund added

--- Assets/Scripts/Logic/ExplodeScript.cs
+++ Assets/Scripts/Logic/ExplodeScript.cs
@@ -7,8 +7,12 @@ public class ExplodeScript : MonoBehaviour
 
     public float explosionDelay = 3f;
     public AudioSource click;
+
+    public GameObject player;
+    public AudioSource explosion;
     void Start()
     {
+      
     }
 
     public void ExplodeAES()
@@ -22,6 +26,7 @@ public class ExplodeScript : MonoBehaviour
     IEnumerator CountdownToExecution()
     {
         click.Play();
+        explosion.Play();
         yield return new WaitForSeconds(explosionDelay);
         SceneManager.LoadScene(1);
     }
