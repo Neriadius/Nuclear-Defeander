5ea1b5102a892c57748fb486e7b022d417bc4917
Neriadius <darin1980museev@gmail.com>
2026-06-11 16:10:46 +0500

    Update Explosion Script

--- Assets/Scripts/Logic/ExplodeScript.cs
+++ Assets/Scripts/Logic/ExplodeScript.cs
@@ -1,11 +1,28 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExplodeScript : MonoBehaviour
 {
+
+    public float explosionDelay = 3f;
+
+    void Start()
+    {
+    }
+
     public void ExplodeAES()
     {
-        //TODO: Add explosion effect here и убрать скрипт ниже
-        Application.Quit();
+       
         Debug.Log("Explode");
+        StartCoroutine(CountdownToExecution());
+
     }
+
+    IEnumerator CountdownToExecution()
+    {
+        yield return new WaitForSeconds(explosionDelay);
+        SceneManager.LoadScene(1);
+    }
+
 }
