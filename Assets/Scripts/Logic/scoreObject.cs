31774db00c7814a89ed2f22263dff0816ee19d83
Neriadius <darin1980museev@gmail.com>
2026-06-18 15:37:36 +0500

    End Screen Update

--- Assets/Scripts/Logic/scoreObject.cs
+++ Assets/Scripts/Logic/scoreObject.cs
@@ -3,12 +3,20 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class scoreObject : MonoBehaviour
 {
     public List<log> logs = new List<log>();
     public float total;
     public int doorCount;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     public void AddScore(log log)
     {
         int i = 0;
@@ -31,6 +39,18 @@ public class scoreObject : MonoBehaviour
     }
     void Update()
     {
-        if(doorCount != 0) { AddScore(new log("Doors", -0.5f*doorCount*Time.deltaTime));  }
+        DoorCounter();
+    }
+
+    void DoorCounter()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            doorCount = 0;
+        }
+        else
+        {
+            AddScore(new log("Doors", -0.5f * doorCount * Time.deltaTime));
+        }
     }
 }
