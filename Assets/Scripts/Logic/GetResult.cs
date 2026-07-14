335175e986decaa9616ef537d76c20e3bb1779e3
Радомир <radikkorol655@gmail.com>
2026-07-14 18:05:17 +0500

    typing

--- Assets/Scripts/Logic/GetResult.cs
+++ Assets/Scripts/Logic/GetResult.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -7,25 +8,35 @@ public class GetResult : MonoBehaviour
     public TMP_Text resultText;
     public TMP_Text valueText;
     public TMP_Text totalText;
+    public float typingSpeed;
     private void Awake()
     {
         scoreManager = FindFirstObjectByType<scoreObject>();
-        foreach (var log in scoreManager.logs)
-        {
-            int score = Mathf.RoundToInt(log.score);
-            resultText.text += log.massage + ": " + "\n";
-            valueText.text += score.ToString() + "\n";
-        }
+
+        StartCoroutine(WriteText());
     }
     private void Update()
     {
         totalText.text = PlayerPrefs.GetInt("score").ToString();
     }
 
-
-    void DisplayResults()
+    IEnumerator WriteText()
     {
-        scoreManager.TotalScore();
-        resultText.text = "Total Score: " + scoreManager.total.ToString();
+        resultText.text = "";
+        valueText.text = "";
+        resultText.maxVisibleCharacters = 0;
+        foreach (var log in scoreManager.logs)
+        {
+            int score = Mathf.RoundToInt(log.score);
+            string line = $"{log.massage}:\n";
+            int start = resultText.text.Length;
+            resultText.text += line;
+            while (resultText.maxVisibleCharacters < start + line.Length)
+            {
+                resultText.maxVisibleCharacters++;
+                yield return new WaitForSeconds(typingSpeed);
+            }
+            valueText.text += score + "\n";
+        }
     }
 }
